592de9db66a4b98cd4c352d19ba2e2bb25a9c14e
dsambugaro <sambugaro.com@gmail.com>
2017-06-15 17:28:08 +0000

    esboço função criar disco + lista sentinela

--- bibliotecas/TAD disco.h
+++ bibliotecas/TAD disco.h
@@ -1,4 +1,12 @@
 
+#ifndef DISCO_H
+#define DISCO_H
+
+#include "lista_sentinela.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct nosetor{
     unsigned long inicio;
     unsigned long fim;
@@ -17,12 +25,12 @@ typedef struct noarquivo{
 typedef struct{
     char nome[40];
     void* disco;
-    NoSetor* livres;
+    Lista* livres;
     NoArquivo* arquivos;
     unsigned long tamDisco;
     unsigned long espacoLivre;
     unsigned long espacoOcupado;
-    unsigned long qtdeArquivos;    
+    unsigned long qtdeArquivos;
 }Disco;
 
 typedef enum{
@@ -33,5 +41,21 @@ typedef enum{
 
 Disco* disco_cria(char* nome, unsigned long tamanho);
 TipoRetorno disco_grava(Disco* d, char* arquivo); //nome arquivo deve conter o caminho absoluto ou relativo do arquivo
-TipoRetorno disco_remove(Disco* d, char* nome) //somente o nome do arquivo sem o caminho
+TipoRetorno disco_remove(Disco* d, char* nome); //somente o nome do arquivo sem o caminho
 TipoRetorno disco_recupera(Disco* d, char* nome, FILE* arquivoFisico);
+
+//Funções
+
+Disco* disco_cria(char* nome, unsigned long tamanho){
+    Disco* d = (Disco*) malloc(sizeof(Disco));
+    strcpy(d->nome, nome);
+    d->disco = (void*) calloc(100, sizeof(0));
+    d->livres = lista_cria();
+    d->tamDisco = 100;
+    d->espacoLivre = 100;
+    d->espacoOcupado = 0;
+    d->qtdeArquivos = 0;
+    return d;
+}
+
+#endif //DISCO_H
---
+++ bibliotecas/lista_sentinela.h
@@ -0,0 +1,99 @@
+
+// Danilo Sambugaro de Carlo
+// RA 1857347
+
+#ifndef LISTA_SENTINELA_H
+#define LISTA_SENTINELA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int T;
+
+typedef struct no{
+    T* ini;
+    T* fim;
+    struct no* prox;
+    struct no* ant;
+}No;
+
+typedef struct{
+    No* sentinela;
+    int tam;
+}Lista;
+
+
+Lista* lista_cria();
+
+void lista_insere(Lista* l, T elemento, int posicao);
+void lista_insere_fim(Lista* l, T elemento);
+
+T* lista_remove1(Lista* l, int posicao);
+int lista_remove2(Lista* l, int posicao, T* endereco);
+T* lista_remove_elemento(Lista* l, T elemento, int (*compara)(void*,void*));
+
+T* lista_busca1(Lista* l, int posicao);
+int lista_busca2(Lista* l, int posicao, T* endereco);
+int lista_contem(Lista* l, T elemento, int (*compara)(void*,void*));
+int lista_posicao(Lista* l, T elemento, int (*compara)(void*,void*));
+
+int lista_tamanho(Lista* l);
+void lista_imprime(Lista* l, int (*imprimeElemento)(void*));
+
+No* novo_No(T elem1, T elem2);
+void posiciona_ponteiro(Lista *l, int posi, No* p);
+
+//FUNÇÕES IMPLEMENTADAS
+
+No* novo_No(T elem1, T elem2){
+    No* n = (No*) malloc(sizeof(No));
+
+    if ((elem1 == -1) || (elem2 == -1)) {
+        n->ini = NULL;
+        n->fim = NULL;
+    } else {
+        n->ini = (T*) malloc(sizeof(T));
+        n->fim = (T*) malloc(sizeof(T));
+        *(n->ini) = elem1;
+        *(n->fim) = elem2;
+    }
+
+    n->prox = NULL;
+    n->ant = NULL;
+    return n;
+}
+
+void posiciona_ponteiro(Lista *l, int posi, No* p){
+    if ((l->tam/2) >= posi) {
+        p = l->sentinela->prox;
+        for (int i = 0; i < posi; i++) {
+            p = p->prox;
+        }
+    } else {
+        p = l->sentinela->ant;
+        for (int i = 0; i < ((l->tam) - (posi)); i++) {
+            p = p->ant;
+        }
+    }
+}
+
+Lista* lista_cria(){
+    Lista* l = (Lista*) malloc(sizeof(Lista));
+    l->sentinela = novo_No(-1, -1);
+    l->tam = 0;
+    return l;
+}
+
+void lista_insere(Lista* l, T ini, T fim, int posicao){
+
+    No* p = NULL;
+
+    posiciona_ponteiro(l, (posicao-1), p);
+    No* novo = novo_No(ini, fim);
+    novo->prox = p->prox;
+    novo->ant = p;
+    p->prox->ant = novo;
+    p->prox = novo;
+}
+
+#endif //LISTA_SENTINELA_H
